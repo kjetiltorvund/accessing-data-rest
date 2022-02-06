40aa8fbf68fe39ca4b9fb7379b163b36a36d14f4
Kjetil Torvund Minde <kjetiltorvund@gmail.com>
2022-02-07 00:01:32 +0100

    Initial commit.

---
+++ src/main/java/no/kjetil/accessingdatarest/AccessingDataRestApp.java
@@ -0,0 +1,11 @@
+package no.kjetil.accessingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataRestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataRestApp.class, args);
+    }
+}
---
+++ src/main/java/no/kjetil/accessingdatarest/Person.java
@@ -0,0 +1,49 @@
+package no.kjetil.accessingdatarest;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
